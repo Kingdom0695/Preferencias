f48e64c699566e23b7b0b8256d1897f203e1071d
Kingdom0695 <kingdom0695@gmail.com>
2018-06-05 08:26:22 -0500

    Se inicio con las preferencias, muestra error en vincular el campo usuario y password. Ya se inicializo el archivo donde se guardaran los usuarios ingresados.

--- app/src/main/java/gatodedany/example/omen/preferencias/registroActivity.java
+++ app/src/main/java/gatodedany/example/omen/preferencias/registroActivity.java
@@ -1,16 +1,22 @@
 package gatodedany.example.omen.preferencias;
 
+import android.app.ActionBar;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class registroActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
 
@@ -25,9 +31,28 @@ public class registroActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent objeto2 = new Intent(registroActivity.this,PrincipalActivity.class);
-
                 startActivity(objeto2);
+
+                guardarPreferencias();
             }
         });
+        cargarPreferencias();
     }
+
+    private void guardarPreferencias() {
+        SharedPreferences preferences = getSharedPreferences("usuarios", Context.MODE_PRIVATE);
+        String user = usuario.getText().toString();
+        String contra = contrasena.getText().toString();
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("Usuario: ", user);
+        editor.putString("Contrasena: ", contra);
+        editor.commit();
+    }
+
+    private void cargarPreferencias() {
+        SharedPreferences preferences = getSharedPreferences("usuarios", Context.MODE_PRIVATE);
+        String user = preferences.getString("Usuario", "No existe la informacion");
+        String contra = preferences.getString("Contrasena", "No existe la informacion");
+    }
+
 }
